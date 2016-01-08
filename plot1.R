f0bf9685dadda7ebb02270c5a7d0f247f238ff6e
Rosa Cueto <rcuet@users.noreply.github.com>
2016-01-08 09:24:27 -0500

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,15 @@
+setwd("D:/Exploratory data analysis/Course Project 1/ExData_Plotting1")
+
+Sys.setlocale("LC_TIME","English")
+colNames <- names(read.table("D:/Exploratory data analysis/Course Project 1/ExData_Plotting1/household_power_consumption.txt", nrow=1,header=TRUE, sep=";"))
+data <- read.table("D:/Exploratory data analysis/Course Project 1/ExData_Plotting1/household_power_consumption.txt",
+                   skip=grep("1/2/2007", readLines("D:/Exploratory data analysis/Course Project 1/ExData_Plotting1/household_power_consumption.txt")),
+                   nrows=2879,col.names = colNames, sep = ";", na.strings="?",
+                   stringsAsFactors=FALSE) 
+
+Date <- paste(data$Date,data$Time)
+data$Date <- strptime(Date,"%d/%m/%Y %H:%M:%S")
+
+png(filename = "plot1.png", width = 480, height = 480, units = "px", bg = "transparent")
+with(data, hist(Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red"))
+dev.off()
